8c76f23ce2a83c48aea2e3ca80ad519f928a6773
LielNagar <104072448+LielNagar@users.noreply.github.com>
2023-07-14 18:14:44 +0300

    homepage for build

--- src/containers/App.js
+++ src/containers/App.js
@@ -610,7 +610,7 @@ class App extends Component {
       let requestModel = { board, dice };
 
       axios
-        .post(`https://localhost:7231/api/Boards`, requestModel)
+        .post(`/cgroup2/test2/tar6/api/Posts/`, requestModel)
         .then((response) => {
           if (response.data == "") {
             this.handleComputerNoMoves(p1IsNext);
